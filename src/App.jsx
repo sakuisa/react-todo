ed4023a002cf53a8a0136a41fb192f115604c73e
sakusan <sssaku.039@gmail.com>
2023-06-14 09:52:04 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,15 @@ export const App = () => {
     setCompleteTodos(newCompleteTodos);
   };
 
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -57,11 +66,13 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button button onClick={() => onClickBack(index)}>
+                  戻す
+                </button>
               </div>
             );
           })}
